eb26e4514a854e3a70bd6eec4e06b041df49362c
Julien Simonet <j.mons54@gmail.com>
2017-03-26 20:19:30 +0200

    debug sound

--- www/app/global/global.module.js
+++ www/app/global/global.module.js
@@ -111,14 +111,13 @@ service('sound', ['$rootScope', 'user', function ($rootScope, user) {
         this.play = function () {
             if (sound) {
                 this.sound.play();
+                this.sound.stop();
             }
             return this;
         };
 
         this.stop = function () {
-            if (this.sound) {
-                this.sound.stop();
-            }
+            
             return this;
         };
 
