0da4ba33c9de1362c435924055b20b453a94b55d
Julien Simonet <julien.simonet.ext@boursorama.fr>
2017-04-04 17:37:05 +0200

    test

--- www/app/home/home.controllers.js
+++ www/app/home/home.controllers.js
@@ -14,10 +14,7 @@ controller('homeCtrl', ['$rootScope', '$scope', '$sce', '$timeout', 'socket', 't
     
     function ($rootScope, $scope, $sce, $timeout, socket, translator, utils, paramsGame, orderByFilter) {
 
-        $rootScope.loading = true;
-
         $scope.$on('$destroy', function() {
-            delete $rootScope.loading;
             socket.emit('leaveHome');
         });
 
@@ -41,8 +38,6 @@ controller('homeCtrl', ['$rootScope', '$scope', '$sce', '$timeout', 'socket', 't
         var createdGames;
         
         socket.on('listGames', function (data) {
-
-            delete $rootScope.loading;
 
             var userGame;
 
